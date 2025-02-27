671298245b4a7db878dfc91041dd70d94cc55476
Vivek6034 <viveksingh6034@gmail.com>
2025-02-27 19:56:26 +0530

    root/quadratic

--- if-else.py
+++ if-else.py
@@ -1,2 +1,20 @@
-#conditional statement
-#if , else , elseif 
+# #conditional statement
+# #if , else , elseif 
+
+# age = int(input("enter your age :\n"))
+# if(age<=12):
+#     print("child age")
+# else:
+#     if(age<=19):
+#         print("teen age")
+#     else:
+#         if(age<=60):
+#             print("young age")
+#         else:
+#             print("old age ")
+
+age = int(input("enter your age :\n"))
+if(age<=0):
+
+
+    
