0d7e1cfa825667b4c4bef9816bd2cc34dada6bf4
Vivek6034 <viveksingh6034@gmail.com>
2025-03-28 20:03:23 +0530

    dict/class

---
+++ class01.py
@@ -0,0 +1,24 @@
+class Myclass:
+    def display():
+        print("method of an class ")
+obj = Myclass
+obj.display()
+
+
+class car:
+    def brand(self):
+        print("ola")
+obj= car()
+obj.brand()
+
+class student:
+    def result(abc):
+        print("250")
+obj= student()
+obj.result()
+
+
+
+
+#wap to create  4 methods inside the  class and  
+# this four methoods of all 4 different userdefine data type
---
+++ dict..py
@@ -0,0 +1,29 @@
+di ={
+    "key1" : "vivek ",
+    "city" : "Bihar",
+    "city" : "indore",
+    "Age" : "24"
+}
+print(type(di))
+print(di)
+# x = di.popitem()
+# print(x)
+y = di.pop("city")
+print(y)
+print(di)
+# di.clear()
+# print(di)
+
+di["city"]= "bhopal"
+print(di)
+
+di["dept"] = "HR"
+print(di)
+
+
+#import first as x 
+#x.func1()
+
+#def func1():
+ #       print(;;;;;;;;;;)
+#func1()
